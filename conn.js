1be377507ca1bf35bbb2800cce0b15e1e4aeca4b
lisa1209 <43108731+lisa1209@users.noreply.github.com>
2019-04-02 14:21:24 +0700

    Add files via upload

---
+++ conn.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.9.2/firebase.js"></script>
+<script>
+ // Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyAxbQm5nrXZpSdc3vur04uBQsJ-AwqKNFk",
+   authDomain: "ginraibot.firebaseapp.com",
+   databaseURL: "https://ginraibot.firebaseio.com",
+   projectId: "ginraibot",
+   storageBucket: "ginraibot.appspot.com",
+   messagingSenderId: "1046614278689"
+ };
+ firebase.initializeApp(config);
+</script>
